c77fd51c57b3bd6a1eee10b4450a42d8ac795fae
Antony <vontonnie@gmail.com>
2020-12-05 16:40:26 +0300

    Add navigate event to LoginViewModel

--- app/src/main/java/com/udacity/shoestore/login/LoginViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/login/LoginViewModel.kt
@@ -1,11 +1,28 @@
 package com.udacity.shoestore.login
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class LoginViewModel : ViewModel(){
+    private val _eventNavigateToWelcomeScreen = MutableLiveData<Boolean>()
+    val eventNavigateToWelcomeScreen:LiveData<Boolean>
+    get() = _eventNavigateToWelcomeScreen
+
 
 
     init {
-        
+
+        _eventNavigateToWelcomeScreen.value = false
+
     }
+
+
+
+    fun navigateToWelcomeScreen(){
+
+        _eventNavigateToWelcomeScreen.value = true
+    }
+
+
 }
