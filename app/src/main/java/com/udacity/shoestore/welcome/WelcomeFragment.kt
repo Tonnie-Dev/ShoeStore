10270b999aaa12c02bdb3993962ab218e669f705
Antony <vontonnie@gmail.com>
2020-12-05 16:45:51 +0300

    Inflate fragment_welcome layout

--- app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/welcome/WelcomeFragment.kt
@@ -6,20 +6,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 
 class WelcomeFragment : Fragment() {
+private lateinit var binding:FragmentWelcomeBinding
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+
+        binding = FragmentWelcomeBinding.inflate(inflater)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        return binding.root
     }
 
 
