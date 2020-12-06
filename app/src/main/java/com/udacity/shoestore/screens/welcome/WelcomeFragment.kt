6fc813615c8ac9945c039887c7a911f3ec2e62cf
Antony <vontonnie@gmail.com>
2020-12-06 17:31:17 +0300

    Add Navigation to Instructions Fragment

--- app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.databinding.FragmentWelcomeBinding
 
 
 class WelcomeFragment : Fragment() {
 private lateinit var binding:FragmentWelcomeBinding
-
+private lateinit var viewModel:WelcomeViewModel
 
 
     override fun onCreateView(
@@ -18,6 +20,30 @@ private lateinit var binding:FragmentWelcomeBinding
 
 
         binding = FragmentWelcomeBinding.inflate(inflater)
+
+
+        //initialize viewModel
+        viewModel = ViewModelProvider(this).get(WelcomeViewModel::class.java)
+
+        //bind layout to viewModel
+        binding.viewModel = viewModel
+
+        //make binding observe LiveData
+
+        binding.lifecycleOwner = viewLifecycleOwner
+
+
+                //observe navigation event
+        viewModel.eventNavigateToInstructionsFragment.observe(viewLifecycleOwner){ eventNavigate ->
+
+            if (eventNavigate){
+
+                findNavController().navigate(WelcomeFragmentDirections.actionWelcomeFragmentToInstructionsFragment())
+
+            }
+        }
+
+
 
         // Inflate the layout for this fragment
         return binding.root
--- app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/screens/welcome/WelcomeViewModel.kt
@@ -1,10 +1,24 @@
 package com.udacity.shoestore.screens.welcome
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class WelcomeViewModel: ViewModel() {
 
+    private val _eventNavigateToInstructionsFragment = MutableLiveData<Boolean>()
+    val eventNavigateToInstructionsFragment:LiveData<Boolean>
+    get() = _eventNavigateToInstructionsFragment
+
     init {
 
+
+        _eventNavigateToInstructionsFragment.value = false
+
+    }
+
+
+    fun onNavigateToInstructionsFragment() {
+        _eventNavigateToInstructionsFragment.value = true
     }
 }
