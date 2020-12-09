6b4b9c5c1b0e0e7d247b1f967493687b71eef9bb
Antony <vontonnie@gmail.com>
2020-12-09 07:57:25 +0300

    Convert fragment_shoe_detail to data binding layout

--- app/src/main/java/com/udacity/shoestore/screens/shoelisting/SharedViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoelisting/SharedViewModel.kt
@@ -5,27 +5,43 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.udacity.shoestore.models.Shoe
 
-class SharedViewModel: ViewModel() {
+class SharedViewModel : ViewModel() {
 
     //EVENT_NAVIGATE_TO_DETAIL_FRAGMENT
     private val _eventNavigateToShoeDetail = MutableLiveData<Boolean>()
-    val eventNavigateToShoeDetail:LiveData<Boolean>
-    get() = _eventNavigateToShoeDetail
+    val eventNavigateToShoeDetail: LiveData<Boolean>
+        get() = _eventNavigateToShoeDetail
 
-//SHOE_LIST
+    //EVENT_NAVIGATE_BACK_TO_SHOE_LISTING
+    private val _eventNavigateBack = MutableLiveData<Boolean>()
+    val eventNavigateBack:LiveData<Boolean>
+    get() = _eventNavigateBack
+
+    //SHOE_LIST
     private val _shoeList = MutableLiveData<MutableList<Shoe>>()
-    val shoeList:LiveData<MutableList<Shoe>>
-    get()= _shoeList
+    val shoeList: LiveData<MutableList<Shoe>>
+        get() = _shoeList
 
     init {
 
         _shoeList.value = mutableListOf()
         _eventNavigateToShoeDetail.value = false
+        _eventNavigateBack . value = false
 
     }
 
 
-    fun onNavigateToShoeDetail() {
+    fun onEditFabButtonClick() {
         _eventNavigateToShoeDetail.value = true
+    }
+
+    fun onSaveButtonClick() {
+
+        _eventNavigateBack.value = true
+
+    }
+
+    fun onCancelButtonClick() {
+        _eventNavigateBack.value = true
     }
 }
